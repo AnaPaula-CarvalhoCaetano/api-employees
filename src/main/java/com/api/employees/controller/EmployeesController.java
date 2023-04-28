397fdce55884707ba9f62f00058b6598320db340
AnaPaula-CarvalhoCaetano <paula.youssef@hotmail.com>
2023-04-28 20:04:25 -0300

    Iniciando classe Controller

---
+++ src/main/java/com/api/employees/controller/EmployeesController.java
@@ -0,0 +1,13 @@
+package com.api.employees.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.employees.service.EmployeesService;
+
+@RestController
+
+public class EmployeesController {
+
+	private EmployeesService employeesService;
+
+}
